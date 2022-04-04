f9e4e267ae42ffdb3da49bd4ef7196b84a0ca64f
Juan Velasco <juanayevela@yahoo.com>
2022-04-04 18:54:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() *10) + 1;
+console.log(random);
+
+let num3 = 12;
+let num4 = 3;
+
+const mod = num3 / num4;
+console.log(mod);
+
+const max = Math.max(0,2,4,6,8,10,12,14,16,18,20);
+console.log(max);
